d7080d76f64716e99b92affe33eae46f33b1e73a
dat nguyen <dat.nguyen@codeenginestudio.com>
2020-10-15 23:11:37 +0700

    [feature/frontend/theme] Setup customized theme

---
+++ src/styles/createMuiTheme.d.ts
@@ -0,0 +1,17 @@
+import { ThemeOptions, Theme } from '@material-ui/core/styles/createMuiTheme';
+
+type Shadow = {
+  widget: string;
+  widgetDark: string;
+  widgetWide: string;
+};
+
+declare module '@material-ui/core/styles/createMuiTheme' {
+  interface ThemeOptions {
+    customShadows: Shadow;
+  }
+
+  interface Theme {
+    customShadows: Shadow;
+  }
+}
