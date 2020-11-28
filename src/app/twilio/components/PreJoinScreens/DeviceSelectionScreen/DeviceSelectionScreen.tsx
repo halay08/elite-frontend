43b1bf1f0d1515ea33072aac0314898166e90311
Truong Vu <truong.vu@codeenginestudio.com>
2020-11-28 17:57:33 +0700

    [feature/vnTf9N2Y] Verify that user allow to access the call or not

--- src/app/twilio/components/PreJoinScreens/DeviceSelectionScreen/DeviceSelectionScreen.tsx
+++ src/app/twilio/components/PreJoinScreens/DeviceSelectionScreen/DeviceSelectionScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   makeStyles,
   Typography,
@@ -7,6 +7,10 @@ import {
   Theme,
   Hidden,
 } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
+import { translations } from 'locales/i18n';
+import Snackbar from '@material-ui/core/Snackbar';
+import Alert from '@material-ui/lab/Alert';
 import LocalVideoPreview from './LocalVideoPreview/LocalVideoPreview';
 import SettingsMenu from './SettingsMenu/SettingsMenu';
 import { Steps } from '../PreJoinScreens';
@@ -15,53 +19,24 @@ import ToggleVideoButton from '../../Buttons/ToggleVideoButton/ToggleVideoButton
 import { useAppState } from '../../../state';
 import useVideoContext from '../../../hooks/useVideoContext/useVideoContext';
 
-const useStyles = makeStyles((theme: Theme) => ({
-  gutterBottom: {
-    marginBottom: '1em',
-  },
-  marginTop: {
-    marginTop: '1em',
-  },
-  deviceButton: {
-    width: '100%',
-    border: '2px solid #aaa',
-    margin: '1em 0',
-  },
-  localPreviewContainer: {
-    paddingRight: '2em',
-    [theme.breakpoints.down('sm')]: {
-      padding: '0 2.5em',
-    },
-  },
-  joinButtons: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    [theme.breakpoints.down('sm')]: {
-      flexDirection: 'column-reverse',
-      width: '100%',
-      '& button': {
-        margin: '0.5em 0',
-      },
-    },
-  },
-  mobileButtonBar: {
-    [theme.breakpoints.down('sm')]: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      margin: '1.5em 0 1em',
-    },
-  },
-  mobileButton: {
-    padding: '0.8em 0',
-    margin: 0,
-  },
-}));
-
 interface DeviceSelectionScreenProps {
   name: string;
   roomName: string;
   setStep: (step: Steps) => void;
 }
+
+const verifyAccess = bearer => {
+  const pageURL = window.location.href;
+  const lastURLSegment = pageURL.substr(pageURL.lastIndexOf('/') + 1);
+  const endpoint = `${process.env.REACT_APP_API_ORIGIN}/call/verify/${lastURLSegment}`;
+
+  return fetch(`${endpoint}`, {
+    headers: {
+      Authorization: bearer,
+      'Content-Type': 'application/json',
+    },
+  }).then(res => res.json());
+};
 
 export default function DeviceSelectionScreen({
   name,
@@ -69,9 +44,23 @@ export default function DeviceSelectionScreen({
   setStep,
 }: DeviceSelectionScreenProps) {
   const classes = useStyles();
+  const [isAllow, setPermission] = useState(false);
+
   const { getToken, isFetching } = useAppState();
   const { connect, isAcquiringLocalTracks, isConnecting } = useVideoContext();
-  const disableButtons = isFetching || isAcquiringLocalTracks || isConnecting;
+  const disableButtons =
+    isFetching || isAcquiringLocalTracks || isConnecting || !isAllow;
+
+  const { t: translator } = useTranslation();
+  const { button: b, prejoinScreen: p } = translations.room;
+
+  useEffect(() => {
+    (async () => {
+      const bearer = localStorage.getItem('token');
+      const { allow } = await verifyAccess(bearer);
+      setPermission(Boolean(allow));
+    })();
+  }, []);
 
   const handleJoin = () => {
     getToken(name, roomName).then(token => connect(token));
@@ -79,6 +68,13 @@ export default function DeviceSelectionScreen({
 
   return (
     <>
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        open={!isAllow}
+        autoHideDuration={6000}
+      >
+        <Alert severity="error">{translator(p.permissionAlert)}</Alert>
+      </Snackbar>
       <Typography variant="h5" className={classes.gutterBottom}>
         Join {roomName}
       </Typography>
@@ -127,7 +123,7 @@ export default function DeviceSelectionScreen({
                 color="primary"
                 onClick={() => setStep(Steps.roomNameStep)}
               >
-                Cancel
+                {translator(b.cancel)}
               </Button>
               <Button
                 variant="contained"
@@ -136,7 +132,7 @@ export default function DeviceSelectionScreen({
                 onClick={handleJoin}
                 disabled={disableButtons}
               >
-                Join Now
+                {translator(b.joinNow)}
               </Button>
             </div>
           </Grid>
@@ -145,3 +141,45 @@ export default function DeviceSelectionScreen({
     </>
   );
 }
+
+const useStyles = makeStyles((theme: Theme) => ({
+  gutterBottom: {
+    marginBottom: '1em',
+  },
+  marginTop: {
+    marginTop: '1em',
+  },
+  deviceButton: {
+    width: '100%',
+    border: '2px solid #aaa',
+    margin: '1em 0',
+  },
+  localPreviewContainer: {
+    paddingRight: '2em',
+    [theme.breakpoints.down('sm')]: {
+      padding: '0 2.5em',
+    },
+  },
+  joinButtons: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column-reverse',
+      width: '100%',
+      '& button': {
+        margin: '0.5em 0',
+      },
+    },
+  },
+  mobileButtonBar: {
+    [theme.breakpoints.down('sm')]: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      margin: '1.5em 0 1em',
+    },
+  },
+  mobileButton: {
+    padding: '0.8em 0',
+    margin: 0,
+  },
+}));
